1c00547fd7c9aaf95166a3fff7ff290ab14ae2a8
is12323 <is12323@B304-9>
2021-10-09 11:48:45 +0700

    Добавьте файлы проекта.

---
+++ KR/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KR
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        PR10ROMEntities context;
+        public MainWindow()
+        {
+            InitializeComponent();
+            context = new PR10ROMEntities();
+            ShowTable();
+        }
+        private void ShowTable()
+        {
+            DataGridRegistrations.ItemsSource = context.Order.ToList();
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            var newRegistration = new Order();
+            context.Order.Add(newRegistration);
+            var win = new WindowAdd(context, newRegistration);
+            win.ShowDialog();
+
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var row = DataGridRegistrations.SelectedItem as Order;
+            if (row == null)
+            {
+                MessageBox.Show("Удалено");
+                return;
+            }
+            MessageBoxResult result = MessageBox.Show("Уверены что хотите удалить?", "Подтверждание удаления",  MessageBoxButton.YesNoCancel, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                context.Order.Remove(row);
+                context.SaveChanges();
+                ShowTable();
+            }
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            Button btnEdit = sender as Button;
+            var currentRegistration = btnEdit.DataContext as Order;
+            var win = new WindowAdd(context, currentRegistration);
+            win.ShowDialog();
+        }
+
+        private void BtnSearch_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnUp_Click(object sender, RoutedEventArgs e)
+        {
+            ShowTable();
+        }
+    }
+}
---
+++ KR/WindowAdd.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KR
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowAdd.xaml
+    /// </summary>
+    public partial class WindowAdd : Window
+    {
+        PR10ROMEntities context;
+        public WindowAdd(PR10ROMEntities context, Order currentRegistration)
+        {
+            InitializeComponent();
+            this.context = context;
+            CmbCustomer.ItemsSource = context.Customer.ToList();
+            CmbAngle.ItemsSource = context.Angle.ToList();
+            CmbColour.ItemsSource = context.Colour.ToList();
+            CmbSize.ItemsSource = context.Size.ToList();
+            this.DataContext = currentRegistration;
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            SaveImage();
+            context.SaveChanges();
+            this.Close();
+        }
+        private void SaveImage()
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "Image files: *.jpg, *.png|*.jpg, *.png";
+            openFile.ShowDialog();
+            if (openFile.FileName.Length != 0)
+            {
+                string nameFile = openFile.FileName;
+                byte[] image = File.ReadAllBytes(nameFile);
+                var reg = (Order)this.DataContext;
+                reg.photo = image;
+                photos.Source = new BitmapImage(new Uri(nameFile));
+
+            }
+        }
+    }
+}
